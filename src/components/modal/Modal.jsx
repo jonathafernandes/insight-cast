078886982b5118fc810a552df1085626c72b988e
jonathafernandes <jonathafernandes@live.com>
2024-02-03 20:06:41 -0300

    animation modal

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,12 +1,24 @@
+import { useEffect } from 'react';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 import styles from "../sass/Modal.module.sass";
 
 import PropTypes from 'prop-types';
 
 export function Modal ({ isOpen, children }) {
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
     if (!isOpen) return null;
 
     return (
-        <div className={styles.modalOverlay}>
+        <div className={styles.modalOverlay}
+            data-aos="fade-zoom-in"
+            data-aos-easing="ease-in-back"
+            data-aos-offset="0"
+            data-aos-duration="200">
             <div className={styles.modal}>
                 <div className={styles.modalContent}>
                     {children}
